396cc20b21d127d5b25ca83dc60b46cedca911eb
Aldiansyah <aldimenur@gmail.com>
2023-06-25 23:45:18 +0700

    add select option dubber

--- src/app/serial-rena-nene/page.tsx
+++ src/app/serial-rena-nene/page.tsx
@@ -10,6 +10,8 @@ function index() {
   const [cardJudul, setCardJudul] = useState("");
   const [form, setForm] = useState([]);
   const [characterCount, setCharacterCount] = useState(1);
+  const [dubber, setDubber] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
   useLayoutEffect(() => {
     if (sessionStorage.getItem("state")) {
@@ -76,7 +78,6 @@ function index() {
                           className="bg-purple-300 text-black uppercase px-4 py-1 rounded-md hover:bg-purple-400"
                           onClick={() => {
                             sessionStorage.setItem("judul", values.judul);
-                            console.log(form);
                           }}
                         >
                           Simpan
@@ -93,20 +94,45 @@ function index() {
                             className="flex gap-2 justify-center px-4"
                             key={i}
                           >
-                            <input
-                              className="px-2 py-1 rounded-md border text-black"
-                              placeholder="Karakter"
-                              name="karakter"
-                              value={values.karakter}
-                              onChange={handleChange}
-                            />
-                            <input
-                              className="px-2 py-1 rounded-md border text-black"
-                              placeholder="Dubber"
-                              name="dubber"
-                              value={values.dubber}
-                              onChange={handleChange}
-                            />
+                            <div>
+                              <h5 className="text-md font-bold text-center text-black">
+                                Karakter {i + 1}
+                              </h5>
+                              <input
+                                className="px-2 py-1 rounded-md border text-black h-10"
+                                placeholder="Karakter"
+                                name="karakter"
+                                value={values.karakter}
+                                onChange={handleChange}
+                              />
+                            </div>
+                            <div>
+                              <h5 className="text-md font-bold text-center text-black">
+                                Dubber {i + 1}
+                              </h5>
+                              <input
+                                className="px-2 py-1 rounded-md border text-black h-10"
+                                placeholder="Dubber"
+                                name="dubber"
+                                value={values.dubber}
+                                onChange={handleChange}
+                              />
+                              <select
+                                className="px-2 py-1 rounded-md border text-black h-10"
+                                onChange={(value) => {
+                                  if (value.target.value === "Tambah") {
+                                    setShowModal(true);
+                                  } else {
+                                    setDubber(value.target.value);
+                                  }
+                                }}
+                              >
+                                <option value="Aldi">Aldi</option>
+                                <option value="Yusuf">Yusuf</option>
+                                <option value="Sakinah">Sakinah</option>
+                                <option value="Tambah">Tambah</option>
+                              </select>
+                            </div>
                           </div>
                         ))}
                         <div className="flex justify-center gap-2">
@@ -152,6 +178,41 @@ function index() {
           </button>
         </Link>
       </main>
+      {showModal ? (
+        <div className="w-screen h-screen absolute bg-transparent top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  backdrop-blur-sm">
+          <div className="z-10 w-[30%] h-[50%] bg-white p-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  rounded-md">
+            <div className="w-full">
+              <div className="flex justify-end">
+                <button
+                  className="bg-red-500 text-white px-2 py-1 rounded-full w-8 h-8 hover:bg-red-700"
+                  onClick={() => setShowModal(!showModal)}
+                >
+                  X
+                </button>
+              </div>
+            </div>
+            <h1 className="text-4xl font-bold text-center text-black">
+              Tambah Dubber
+            </h1>
+            <div className="flex flex-col gap-2">
+              <input
+                className="px-2 py-1 rounded-md border text-black h-10"
+                placeholder="Nama Dubber"
+                value={dubber}
+                onChange={(value) => setDubber(value.target.value)}
+              />
+              <button
+                className="bg-purple-300 text-black uppercase px-4 py-1 rounded-md hover:bg-purple-400"
+                onClick={() => {}}
+              >
+                Tambah
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
